bceb9f9d0223952f36c57bdb25d4950ffbcf256b
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-11-05 18:55:17 +0200

    feat: :sparkles: init

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="/">Home</a></li>
+				<li><a href="/about">About</a></li>
+				<li><a href="/portfolio">Portfolio</a></li>
+				<li><a href="/contact">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+</template>
+
+<style scoped>
+/* Add your styles here */
+</style>
